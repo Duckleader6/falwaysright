a10571ef43c364ea163b37df3cf114dfe1741f4d
Duckleader6 <duckleader6@gmail.com>
2026-05-16 00:40:35 +0000

    tweaked percentage and made it rerunnable

--- alwaysright.py
+++ alwaysright.py
@@ -5,9 +5,14 @@ run = True
 a = random.randint(1, 100)
 b = random.randint(1, 100)
 c = random.randint(1, 100)
+d = random.randint(1, 100)
+e = random.randint(1, 100)
 
 async def main():
-    hi = str(await input("What fact would you like to check?: "))
-    print("That is:", max(a, b, c), "% right.")
+    while True:
+        hi = str(await input("What fact would you like to check?: "))
+        print("That is:", max(a, b, c, d, e), "% right.")
+        if await input("Do you want to convert something else? (y/n)\n") == "n":
+            break
 
 globals()["main"] = main
